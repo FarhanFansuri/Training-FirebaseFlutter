49d5387e838be4ede2ae638df3e1c8fde43f4cb7
Farhan Fansuri <muhfarhan912391@gmail.com>
2023-03-02 14:45:19 +0700

    penambahan authentikasi-01

---
+++ myapp/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home();
+  }
+}
